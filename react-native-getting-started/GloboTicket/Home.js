5286c79bdd268de16311a2d5154b2749d91c0563
sguerin <sguerin@guess-engineering.com>
2021-12-14 20:55:13 +0100

    build fix

--- react-native-getting-started/GloboTicket/Home.js
+++ react-native-getting-started/GloboTicket/Home.js
@@ -1,49 +1,67 @@
 import React from 'react';
-import {View, Image, Text, StyleSheet} from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import Menu from './Menu';
 
-const Home = props => {
-  return (
-    <View style={styles.container}>
-      <Image
-        style={styles.globologo}
-        source={require('./images/_Export_globoticket-bug-black.png')}
-      />
-      <Text style={styles.title}>Welcome To GloboTicket</Text>
-      <Text style={styles.subtitle}>{props.username}</Text>
-      <View style={styles.textcontainer}>
-        <Text style={styles.content}>{introText}</Text>
-      </View>
-    </View>
-  );
-};
+const Home =(props) => {
+    return (
+        <View style={styles.container}>
+            <Image 
+                style={styles.globologo}
+                source={require('./images/_Export_globoticket-bug-black.png')}    
+            />
+            <Text style={styles.title}>Welcome TO GloboTicket</Text>
+            <Text style={styles.subtitle}>{props.username}</Text>
+            <Image 
+                style={styles.heroimage}
+                source={require('./images/boxing.jpg')}
+            />
 
-const introText = `Quid ad utilitatem tantae pecuniae? Ut nemo dubitet, eorum omnia officia quo spectare, quid sequi, quid fugere debeant? Eodem modo is enim tibi nemo dabit, quod, expetendum sit, id esse laudabile. Qui-vere falsone, quaerere mittimus-dicitur oculis se privasse; Dolor ergo, id est summum malum, metuetur semper, etiamsi non aderit; Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?`;
+            <View style={styles.textcontainer}>
+                <Text style={styles.content}>{introText}</Text>
+            </View>
+            <View style={styles.menu}>
+                <Menu />
+            </View>   
+        </View>
+    );
+}
+
+const introText = `Are you ready for the best events? Whether you are into sports, music, or the most amazing seminars we have got you covered. Get ready to purchase great tickets at the best prices. Events are in-person and virtual.`;
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: 20,
-  },
-  textcontainer: {
-    textAlign: 'center',
-    paddingTop: 10,
-  },
-  globologo: {
-    height: 100,
-    width: 150,
-  },
-  title: {
-    fontFamily: 'Ubuntu-Regular',
-  },
-  subtitle: {
-    paddingTop: 5,
-    fontFamily: 'Ubuntu-Regular',
-  },
-  content: {
-    fontFamily: 'Ubuntu-Light',
-    fontWeight: '300',
-  },
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+        flex: 1
+    },
+    textcontainer: {
+        padding: 20
+    },
+    globologo: {
+        height: 100,
+        width: 150
+    },
+    title: {
+        fontFamily: 'Ubuntu-Regular'
+    },
+    subtitle: {
+        paddingTop: 5,
+        fontFamily: 'Ubuntu-Regular'
+    },
+    content: {
+        fontFamily: 'Ubuntu-Light',
+        fontWeight: '300'
+    },
+    heroimage: {
+        height: 170,
+        width: '100%'
+    },
+    menu: {
+        position: 'absolute',
+        bottom: 10
+    }
 });
 
 export default Home;
