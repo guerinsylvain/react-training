6beac108e10afb72467494226583dbaa3183eb56
sguerin <sguerin@guess-engineering.com>
2021-12-27 17:56:16 +0100

    News component added

--- react-native-getting-started/GloboTicket/App.js
+++ react-native-getting-started/GloboTicket/App.js
@@ -7,6 +7,7 @@ import Home from './Home';
 import Tickets from './Tickets';
 import Contact from './Contact';
 import TicketPurchase from './TicketPurchase';
+import News from './News';
 
 const Stack = createStackNavigator();
 
@@ -47,6 +48,15 @@ const App: () => React$Node = () => {
               headerTitleAlign: 'center',
               headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
               headerTitle: 'Purchase Tickets',
+            }}
+          />
+          <Stack.Screen
+            name="News"
+            component={News}
+            options={{
+              headerTitleAlign: 'center',
+              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
+              headerTitle: 'Latest News',
             }}
           />
         </Stack.Navigator>
