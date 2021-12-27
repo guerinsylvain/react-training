2902866bba8d187ada058a8f203fe86c74d79c83
sguerin <sguerin@guess-engineering.com>
2021-12-27 17:44:05 +0100

    ticketpurchase added

--- react-native-getting-started/GloboTicket/App.js
+++ react-native-getting-started/GloboTicket/App.js
@@ -1,10 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
 import Tickets from './Tickets';
+import Contact from './Contact';
+import TicketPurchase from './TicketPurchase';
 
 const Stack = createStackNavigator();
 
@@ -13,34 +15,44 @@ const App: () => React$Node = () => {
     <>
       <StatusBar barStyle="dark-content" hidden />
       <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName='Home'
-          headerMode='screen'
-        >
-          <Stack.Screen 
-            name='Home'
+        <Stack.Navigator initialRouteName="Home" headerMode="screen">
+          <Stack.Screen
+            name="Home"
             options={{
-              headerShown: false
-            }}
-          >
-            {(props) => <Home {...props} username='Sports Fan' />}
+              headerShown: false,
+            }}>
+            {props => <Home {...props} username="Sports Fan" />}
           </Stack.Screen>
-          <Stack.Screen 
-            name='Tickets'
+          <Stack.Screen
+            name="Tickets"
             component={Tickets}
             options={{
               headerTitleAlign: 'center',
-              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'}
+              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
             }}
           />
-
+          <Stack.Screen
+            name="Contact"
+            component={Contact}
+            options={{
+              headerTitleAlign: 'center',
+              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
+              headerTitle: 'Contact Us',
+            }}
+          />
+          <Stack.Screen
+            name="Purchase"
+            component={TicketPurchase}
+            options={{
+              headerTitleAlign: 'center',
+              headerTitleStyle: {fontFamily: 'Ubuntu-Regular'},
+              headerTitle: 'Purchase Tickets',
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
-      
     </>
   );
 };
-
-
 
 export default App;
--- react-native-getting-started/GloboTicket/Menu.js
+++ react-native-getting-started/GloboTicket/Menu.js
@@ -1,38 +1,40 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
 const Menu = () => {
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  return (
+    <View style={styles.menu}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Tickets')}
+        style={styles.button}>
+        <Text style={styles.buttontext}>Events</Text>
+      </TouchableOpacity>
 
-    return(
-        <View style={styles.menu}>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Tickets')}
-                style={styles.button}
-            >
-                <Text style={styles.buttontext}>Events</Text>
-            </TouchableOpacity>    
-        </View>
-
-    );
-}
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Contact')}
+        style={styles.button}>
+        <Text style={styles.buttontext}>Contact Us</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    menu: {
-        flexDirection: 'row'
-    },
-    button: {
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        marginRight: 5,
-        marginLeft: 5
-    },
-    buttontext: {
-        fontFamily: 'Ubuntu-Regular'
-    }
+  menu: {
+    flexDirection: 'row',
+  },
+  button: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    marginRight: 5,
+    marginLeft: 5,
+  },
+  buttontext: {
+    fontFamily: 'Ubuntu-Regular',
+  },
 });
 
 export default Menu;
-
